defaeecda0ab1f777a2a95e2099d7b5bf9d30e07
jason <jason2580134@outlook.com>
2023-05-23 11:05:04 -0700

    out

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from bs4 import BeautifulSoup
+from rich.console import Console
+from rich.theme import Theme
 
 IG_NAME_CLASS = "x9f619 xjbqb8w x1rg5ohu x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x6s0dn4 x1oa3qoh x1nhvcw1"
 
@@ -23,8 +25,26 @@ def get_user_set(file_path: str) -> list:
 
     return user_set
 
+def write_output(intersection: list, users: list):
+    out_pattern = '<------------------------------------------------------------------------------>'
+
+    with open('out.txt', 'w') as file:
+        sys.stdout = file
+        i_output = ''
+        u_output = ''
+
+        for u in intersection:
+            i_output = i_output + u + '\n'
+
+        for u in users:
+            u_output = u_output + u + ' '
+
+        print(f'Common Followers: {u_output}\n')
+        print(f'{out_pattern}\n\n{i_output}\n\n{out_pattern}')
+
 def exit_unread():
-    print("No files read")
+    error_theme = Theme({'error': 'bold red'})
+    Console(theme=error_theme).print("[error]ERROR: No files were read![/error]")
     sys.exit(1)
 
 def main():
@@ -73,11 +93,9 @@ def main():
             if excluded_user in intersection:
                 intersection.remove(excluded_user)
 
-    print(intersection)
-    print(exclude_set)
+    write_output(intersection, file_names)
 
     sys.exit(0)
-
 
 if __name__ == "__main__":
     main()
